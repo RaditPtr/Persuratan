995d25c412b3a0531b2619e338e93165bbd4c3b7
RaditPtr <radityoptr@gmail.com>
2023-10-13 07:02:15 +0700

    First Commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,91 @@
+<html>
+
+<head>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>@yield('title')</title>
+    @yield('header')
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+
+        /* Show it is fixed to the top */
+        body {
+            min-height: 75rem;
+            padding-top: 4.5rem;
+        }
+
+        nav {
+            background-color: #9DC183;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md fw-semibold fixed-top text-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Persuratan</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('dashboard/surat') }}">Dashboard</a>
+                    </li>
+                    @if (Auth::check() && Auth::user()->role == 'admin')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('admin/user') }}">Manage User</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('jenis/surat') }}">Jenis Surat</a>
+                        </li>
+                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('transaksi/surat') }}">Transaksi Surat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/logout') }}" tabindex="-1">Logout</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        @include('layout.flash-message')
+        @yield('content')
+    </div>
+</body>
+<footer>
+    @yield('footer')
+</footer>
+<html>
